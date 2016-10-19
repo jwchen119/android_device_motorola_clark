54c4e3cdb880e311d02c8c9c38edafebeae37948
jwchen119 <jwchen119@gmail.com>
2016-10-19 20:56:57 +0800

    Rename  clark stuff to kinzie and change config fit to kinzie | special thanks to @Arasthel and @xadro

--- cm.mk
+++ cm.mk
@@ -13,9 +13,9 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_clark
+PRODUCT_NAME := cm_kinzie
 PRODUCT_BRAND := Android
-PRODUCT_DEVICE := clark
-PRODUCT_MODEL := XT1575
+PRODUCT_DEVICE := kinzie
+PRODUCT_MODEL := XT1581
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
